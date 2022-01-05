2af4fd26d64f7ff3ccf503f565d1e7ff6a3d264e
Rob <robburson07@gmail.com>
2022-01-05 11:12:33 -0600

    added comment routes

---
+++ controllers/comment-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { join } = require('path/posix');
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req,res) => {
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req,res) => {
+    if(req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.body.user_id,
+            post_id: req.body.post_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
