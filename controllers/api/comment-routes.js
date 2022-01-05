33cf366d7572b186271c34ea9862182ae7507871
Adam Field <adamjfield@gmail.com>
2022-01-05 12:13:58 -0600

    added comments and made dashboard more responsive

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -15,15 +15,15 @@ router.get('/', (req,res) => {
 router.post('/', withAuth, (req,res) => {
     if(req.session) {
         Comment.create({
-            comment_text: req.body.comment_text,
-            user_id: req.body.user_id,
-            post_id: req.body.post_id
+          comment_text: req.body.comment_text,
+          user_id: req.session.user_id,
+          post_id: req.body.post_id,
         })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
+          .then(dbCommentData => res.json(dbCommentData))
+          .catch(err => {
             console.log(err);
             res.status(400).json(err);
-        });
+          });
     }
 });
 module.exports = router;
