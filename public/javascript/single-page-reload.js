f338b1dd20c9f931ca471f0f6d3149736a937b02
Adam Field <adamjfield@gmail.com>
2022-01-06 20:08:00 -0600

    fixed comment link

---
+++ public/javascript/single-page-reload.js
@@ -0,0 +1,15 @@
+// Fixed url have using the comment link
+setTimeout(() => {
+  const commentUrl = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  if (commentUrl === '#comment') {
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 2
+    ];
+
+    const newUrl = '/blog/post/' + id;
+    history.replaceState({}, '', newUrl);
+  }
+}, 5);
